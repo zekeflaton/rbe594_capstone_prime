5dbdbfaab6f430b4cfcd0b10b62cb2e151b8b782
zekeflaton <79118314+zekeflaton@users.noreply.github.com>
2023-03-11 13:37:37 -0500

    add docstring and comments

--- src/generate_warehouse_map.py
+++ src/generate_warehouse_map.py
@@ -4,8 +4,19 @@ import numpy as np
 
 
 def generate_warehouse_numpy_map(map_file='../src/warehouse.csv'):
+    """
+    Uses the csv warehouse map file to generate a human friendly image
+
+    :param str map_file: filepath for the map csv
+    :return: np.ndarray ary_map: warehouse map in a numpy array
+    """
+    # Import the CSV as a numpy array
     csv_map = pd.read_csv(map_file, header=None).to_numpy()
+
+    # Generate a 3D numpy array to save to an image later.  Default values are white (255, 255, 255)
     ary_map = np.ones((csv_map.shape[0], csv_map.shape[1], 3), dtype='uint8') * 255
+
+    # For any location where there is a shelf turn that pixel black (0, 0, 0).
     for i in range(csv_map.shape[0]):
         for j in range(csv_map.shape[1]):
             if csv_map[i, j] == 255:
