01d4f1b6f170dc7bf4e49421e0e203130c5ccdc8
zekeflaton <79118314+zekeflaton@users.noreply.github.com>
2023-03-13 22:10:46 -0400

    updates compute time and path efficiency scripts to output bar graphs

--- scripts/compute_time_analysis.py
+++ scripts/compute_time_analysis.py
@@ -12,19 +12,23 @@ from src.motion_planners import (
 )
 
 
-def main():
+def main(generate_data=False):
+    """
+    :param bool generate_data: Should the sim be run to generate data
+    """
     metrics_file_path = "../results"
-    write_line_to_file(os.path.join(metrics_file_path, "compute_time_analysis.csv"),
-                       ["motion_planner", "time_to_compute_path"], "w")
-    motion_planners = [
-        AStarPlanner(),
-        BreadthFirstSearchPlanner(),
-        DepthFirstSearchPlanner(),
-        RandomPlanner()
-    ]
-    for motion_planner in motion_planners:
-        for num_robots in range(20, 30):
-            run_analysis_sim(num_of_robots=num_robots, shelves_to_grab=30, motion_planner=motion_planner, metrics_file_path=metrics_file_path)
+    if generate_data:
+        write_line_to_file(os.path.join(metrics_file_path, "compute_time_analysis.csv"),
+                           ["motion_planner", "time_to_compute_path"], "w")
+        motion_planners = [
+            AStarPlanner(),
+            BreadthFirstSearchPlanner(),
+            DepthFirstSearchPlanner(),
+            RandomPlanner()
+        ]
+        for motion_planner in motion_planners:
+            for num_robots in range(20, 30):
+                run_analysis_sim(num_of_robots=num_robots, shelves_to_grab=30, motion_planner=motion_planner, metrics_file_path=metrics_file_path)
 
     df = pd.read_csv(os.path.join(metrics_file_path, "compute_time_analysis.csv"))
     df = df[df.time_to_compute_path != 0]
@@ -32,19 +36,22 @@ def main():
     df["motion_planner"].replace("BreadthFirstSearchPlanner", "BFS", inplace=True)
     df["motion_planner"].replace("DepthFirstSearchPlanner", "DFS", inplace=True)
     df["motion_planner"].replace("RandomPlanner", "Random", inplace=True)
-
     print(df.head())
+
     mean = df.groupby(["motion_planner"]).mean()
     print(mean)
 
-    fig = mean.plot(
+    fig = mean.plot.bar(
         xlabel='Motion Planner',
         ylabel='Average Time to Compute Path (seconds)',
         title="Average Compute Time vs Motion Planner",
-        legend=False)
+        legend=False,
+        rot=0
+    )
 
     fig.figure.savefig('../results/compute_time_analysis.png')
 
 
 if __name__ == "__main__":
-    main()
+    generate_data = False
+    main(generate_data=generate_data)
