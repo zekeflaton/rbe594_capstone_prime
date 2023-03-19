8ce367dc12581c2d77d84d1a5a7f0069f4561880
hark001 <harishkumar.ramadhas@gmail.com>
2023-03-19 13:55:25 -0400

    Initial update on gazebo simulation with few bugs sorted

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/harish/wpi/caps_proj_kiva/src
+# Build directory: /home/harish/wpi/caps_proj_kiva/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_control")
+subdirs("robot_description")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/harish/wpi/caps_proj_kiva/devel:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/home/harish/wpi/caps_proj_kiva/devel/lib:/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu'
+export PKG_CONFIG_PATH='/home/harish/wpi/caps_proj_kiva/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig:/opt/ros/noetic/lib/x86_64-linux-gnu/pkgconfig'
+export PWD='/home/harish/wpi/caps_proj_kiva/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/harish/wpi/caps_proj_kiva/devel/share/common-lisp'
