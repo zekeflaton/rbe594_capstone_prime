6eff7cb16811739b65ecb582f02648d6eaa7baf1
RoboCD <chrisjd91@gmail.com>
2023-03-20 21:50:26 -0400

    Removed robot_control2 and changed robot_description to ROS2 python package

--- ros2_ws/src/robot_description/setup.py
+++ ros2_ws/src/robot_description/setup.py
@@ -1,8 +1,9 @@
 import os
 from glob import glob
 from setuptools import setup
+from setuptools import find_packages
 
-package_name = 'robot_description2'
+package_name = 'robot_description'
 
 setup(
     name=package_name,
@@ -12,7 +13,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
+        (os.path.join('share', package_name, 'urdf', 'robot_parts'), glob('urdf/robot_parts/*')),
+        (os.path.join('share', package_name, 'urdf', 'sensors'), glob('urdf/sensors/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf.xarco')),
+        (os.path.join('share', package_name), glob('world/*')),
+        (os.path.join('share', package_name), glob('rviz/*'))
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
