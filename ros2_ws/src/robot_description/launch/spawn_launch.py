934345b9f02e307bae7d730b88b2de3c53241ec0
RoboCD <chrisjd91@gmail.com>
2023-03-23 00:34:46 -0400

    Got gazebo and rviz to launch, but no robot model seen

--- ros2_ws/src/robot_description/launch/spawn_launch.py
+++ ros2_ws/src/robot_description/launch/spawn_launch.py
@@ -2,13 +2,33 @@ import launch
 from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 import os
+import xacro
+
 
 def generate_launch_description():
     pkg_name = 'robot_description'
     pkg_share = launch_ros.substitutions.FindPackageShare(package=pkg_name).find(pkg_name)
     default_model_path = os.path.join(pkg_share, 'urdf/robot.urdf.xacro')
-    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz/robotmodel.rviz')
+    doc = xacro.process_file(default_model_path, mappings={'radius': '0.9'})
+    world_path=os.path.join(pkg_share, 'world/warehous.world')
 
+# path_to_urdf = get_package_share_path('pr2_description') / 'robots' / 'pr2.urdf.xacro'
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{
+            'robot_description': launch_ros.descriptions.ParameterValue(
+                Command(['xacro ',
+                         str(default_model_path),
+                         ' sensor_rplidar:=true',
+                         ' sensor_lidar:=false',
+                         ' sensor_imu:=true',
+                        ' sensor_camera:=true']),
+                    value_type=str
+            )
+        }]
+    )
 
     spawn_entity = launch_ros.actions.Node(
         package='gazebo_ros',
@@ -16,29 +36,30 @@ def generate_launch_description():
         arguments=['-entity', 'my_robot', '-topic', 'robot_description'],
         output='screen'
     )
-    robot_state_publisher_node = launch_ros.actions.Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
-    )
+    # robot_state_publisher_node = launch_ros.actions.Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    # )
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    )
+    # joint_state_publisher_gui_node = launch_ros.actions.Node(
+    #     package='joint_state_publisher_gui',
+    #     executable='joint_state_publisher_gui',
+    #     name='joint_state_publisher_gui',
+    #     condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+    # )
+
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+        # arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
 
     return launch.LaunchDescription([
@@ -46,10 +67,14 @@ def generate_launch_description():
                                             description='Flag to enable joint_state_publisher_gui'),
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                            description='Absolute path to rviz config file'),
+        # launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+        #                                     description='Absolute path to rviz config file'),
+        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
+                                      output='screen'),
+        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path],
+                                      output='screen'),
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
+        # joint_state_publisher_gui_node,
         robot_state_publisher_node,
         rviz_node,
         spawn_entity
--- ros2_ws/src/robot_description/setup.py
+++ ros2_ws/src/robot_description/setup.py
@@ -4,23 +4,43 @@ from setuptools import setup
 from setuptools import find_packages
 
 package_name = 'robot_description'
-
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=[package_name],
-    data_files=[
+#https://answers.ros.org/question/397319/how-to-copy-folders-with-subfolders-to-package-installation-path/
+data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
-        (os.path.join('share', package_name, 'urdf', 'robot_parts'), glob('urdf/robot_parts/*')),
-        (os.path.join('share', package_name, 'urdf', 'sensors'), glob('urdf/sensors/*')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf.xarco')),
-        (os.path.join('share', package_name), glob('world/*')),
-        (os.path.join('share', package_name), glob('rviz/*'))
+        (os.path.join('share', package_name, 'world'), glob('world/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*'))
+    ]
+def package_files(data_files, directory_list):
 
-    ],
+    paths_dict = {}
+
+    for directory in directory_list:
+
+        for (path, directories, filenames) in os.walk(directory):
+
+            for filename in filenames:
+
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, path)
+
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+
+                else:
+                    paths_dict[install_path] = [file_path]
+
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+
+    return data_files
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=package_files(data_files, ['models/', 'urdf/']),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='chris',
