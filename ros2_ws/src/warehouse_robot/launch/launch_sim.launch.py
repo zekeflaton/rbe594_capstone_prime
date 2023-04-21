98051382aeaee7629fd22c49b9036deed628dd57
Robert Menna <robjmenna@gmail.com>
2023-04-21 10:39:09 -0400

    fixed piston control

--- ros2_ws/src/warehouse_robot/launch/launch_sim.launch.py
+++ ros2_ws/src/warehouse_robot/launch/launch_sim.launch.py
@@ -61,11 +61,11 @@ def generate_launch_description():
     #     arguments=["diff_cont", "--controller-manager-timeout", "30"],
     # )
 
-    # joint_broad_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     arguments=["joint_broad", "--controller-manager-timeout", "30"],
-    # )
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad", "--controller-manager-timeout", "30"],
+    )
 
 
     # Code for delaying a node (I haven't tested how effective it is)
@@ -90,11 +90,11 @@ def generate_launch_description():
     #     name='joint_state_publisher_gui'
     # )
 
-    # joint_piston_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     arguments=["piston_cont", "--controller-manager-timeout", "30"],
-    # )
+    joint_piston_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["piston_cont", "--controller-manager-timeout", "30"],
+    )
 
 
     # Launch them all!
@@ -105,6 +105,6 @@ def generate_launch_description():
         gazebo,
         spawn_entity,
         # diff_drive_spawner,
-        # joint_broad_spawner,
-        # joint_piston_spawner,
+        joint_broad_spawner,
+        joint_piston_spawner,
     ])
